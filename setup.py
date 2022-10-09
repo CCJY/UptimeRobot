d602fd90aed9080a36b63c7ab8ee6730cd803b8b
Junyoung Choi <ccjy1113@gmail.com>
2022-10-09 00:24:47 -0400

    Add setup.py --version=1.0

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='UptimeRobot',
+      version='1.0',
+      description='Diversio BE exercise 2',
+      author='Junyoung Choi',
+      author_email='junyoung.choi.dev@gmail.com',
+      packages=['diversio'],
+      )
