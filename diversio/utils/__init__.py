2eb96a69cd2c8e4d4803af034fe1f60e936b5fe4
Junyoung Choi <ccjy1113@gmail.com>
2022-10-09 15:44:18 -0400

    🔥 Remove unnecessary code

--- diversio/utils/__init__.py
+++ diversio/utils/__init__.py
@@ -1,31 +1,3 @@
-class _dict(dict):
-    """dict like object that exposes keys as attributes"""
-
-    def __getattr__(self, key):
-        ret = self.get(key)
-        # "__deepcopy__" exception added to fix frappe#14833 via DFP
-        if not ret and key.startswith("__") and key != "__deepcopy__":
-            raise AttributeError()
-        return ret
-
-    def __setattr__(self, key, value):
-        self[key] = value
-
-    def __getstate__(self):
-        return self
-
-    def __setstate__(self, d):
-        self.update(d)
-
-    def update(self, d):
-        """update and return self -- the missing dict feature in python"""
-        super(_dict, self).update(d)
-        return self
-
-    def copy(self):
-        return _dict(dict(self).copy())
-
-
 def median(datas):
     length = len(datas)
     if (length % 2) == 0:
