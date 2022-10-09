2f754a7c1a4303d02e7e778a80e5bdc0cc201f9f
Junyoung Choi <ccjy1113@gmail.com>
2022-10-09 01:56:48 -0400

    Update summary function

--- diversio/model/monitor_stats.py
+++ diversio/model/monitor_stats.py
@@ -24,25 +24,30 @@ class MonitorStats(Base):
             raise ValueError("The monitor field is not instance of monitor")
 
     def summary(self):
-        highest, lowest, median_time = self.get_monitor().get_response_times_summary()
+        monitor = self.get_monitor()
+        highest, lowest, median_time, average_time = monitor.get_response_times_summary()
         message = "{0} received highest ping on {1} with value {2}ms in last {3} days" \
-            .format(self.monitor.name,
+            .format(monitor.name,
                     highest.get_str_local_datetime(
                         self.get_timezone()),
                     highest.value,
                     highest.get_last_days(self.get_timezone()))
         print(message)
         message = "{0} received lowest ping on {1} with value {2}ms in last {3} days" \
-            .format(self.monitor.name,
+            .format(monitor.name,
                     lowest.get_str_local_datetime(
                         self.get_timezone()),
                     lowest.value,
                     lowest.get_last_days(self.get_timezone()))
         print(message)
         message = "{0} received median ping on {1} with value {2}ms in last {3} days" \
-            .format(self.monitor.name,
+            .format(monitor.name,
                     median_time.get_str_local_datetime(
                         self.get_timezone()),
                     median_time.value,
                     median_time.get_last_days(self.get_timezone()))
+        print(message)
+
+        message = "{0} average ping is {1}ms".format(
+            monitor.get_name(), average_time)
         print(message)
