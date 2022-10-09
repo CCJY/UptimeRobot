810fb196e961b8b6616fbe238fca744820736739
Junyoung Choi <ccjy1113@gmail.com>
2022-10-09 01:32:07 -0400

    Add a new method _get_datetime

--- diversio/model/response_times.py
+++ diversio/model/response_times.py
@@ -2,4 +2,17 @@ from diversio.model.base import Base
 
 
 class ResponseTime(Base):
-    pass
+    strptime_with_tz_format = "%B %d, %Y, %H:%M%z"
+    strptime_format = "%B %d, %Y, %H:%M"
+    strftime_format = "%B %d, %Y, %H:%M"
+
+    def _get_datetime(self, timezone: str = None) -> datetime.datetime:
+        if not self.datetime:
+            raise ValueError("The datetime field is none")
+        if timezone:
+            return datetime.datetime.strptime(
+                self.datetime + timezone, self.strptime_with_tz_format
+            )
+        return datetime.datetime.strptime(
+            self.datetime, self.strptime_format
+        )
