6400648871b9fbd175bfe3518670ceb7ea6a2eb8
Junyoung Choi <ccjy1113@gmail.com>
2022-10-09 01:37:59 -0400

    ⚰️ Remove str_response_time_detail and ♻️ refactor code

--- diversio/model/monitor_stats.py
+++ diversio/model/monitor_stats.py
@@ -1,5 +1,6 @@
 import datetime
 from diversio.model.base import Base
+from diversio.model.monitor import Monitor
 from diversio.model.response_times import ResponseTime
 from diversio.utils import median
 
@@ -14,30 +15,34 @@ class MonitorStats(Base):
 
         return "+00:00"
 
-    strptime_format = "%B %d, %Y, %H:%M%z"
-    strftime_format = "%B %d, %Y, %H:%M"
-
-    def str_response_time_detail(self, response_time: ResponseTime):
-        if response_time.datetime:
-            current_time = datetime.datetime.now(datetime.timezone.utc)
-            dt = datetime.datetime.strptime(
-                response_time.datetime + self.get_timezone(), self.strptime_format)
-            last_days = (current_time - dt).days
-
-            message = "{0} received highest ping on {1} with value {2}ms in last {3} days" \
-                .format(self.monitor.name,
-                        datetime.datetime.strftime(
-                            dt.astimezone(), self.strftime_format),
-                        # response_time.datetime,
-                        response_time.value,
-                        last_days)
-            return message
-
-        return ""
+    def get_monitor(self) -> Monitor:
+        if not self.monitor:
+            raise ValueError("The monitor field is none")
+        if isinstance(self.monitor, Monitor):
+            return self.monitor
+        else:
+            raise ValueError("The monitor field is not instance of monitor")
 
     def summary(self):
-        highest, lowest, median_time = self.monitor.get_response_times_summary()
-
-        print(self.str_response_time_detail(highest))
-        print(self.str_response_time_detail(lowest))
-        print(self.str_response_time_detail(median_time))
+        highest, lowest, median_time = self.get_monitor().get_response_times_summary()
+        message = "{0} received highest ping on {1} with value {2}ms in last {3} days" \
+            .format(self.monitor.name,
+                    highest.get_str_local_datetime(
+                        self.get_timezone()),
+                    highest.value,
+                    highest.get_last_days(self.get_timezone()))
+        print(message)
+        message = "{0} received lowest ping on {1} with value {2}ms in last {3} days" \
+            .format(self.monitor.name,
+                    lowest.get_str_local_datetime(
+                        self.get_timezone()),
+                    lowest.value,
+                    lowest.get_last_days(self.get_timezone()))
+        print(message)
+        message = "{0} received median ping on {1} with value {2}ms in last {3} days" \
+            .format(self.monitor.name,
+                    median_time.get_str_local_datetime(
+                        self.get_timezone()),
+                    median_time.value,
+                    median_time.get_last_days(self.get_timezone()))
+        print(message)
